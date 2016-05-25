5910a6db55e87aa5b35f28726badafea239b39f1
Nishanth Kumar Gunda <nishanthkumarg@google.com>
2016-05-24 18:23:16 -0700

    First commit

---
+++ app/src/main/java/com/example/naveenkumark/testjava8/FunctionalInterface.java
@@ -0,0 +1,8 @@
+package com.example.naveenkumark.testjava8;
+
+/**
+ * Created by naveenkumark on 4/20/16.
+ */
+public interface FunctionalInterface {
+    boolean getId(Person P);
+}
---
+++ app/src/main/java/com/example/naveenkumark/testjava8/Person.java
@@ -0,0 +1,15 @@
+package com.example.naveenkumark.testjava8;
+
+/**
+ * Created by naveenkumark on 4/20/16.
+ */
+public class Person {
+    Person(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+    String name;
+    int age;
+    int id;
+}
